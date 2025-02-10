3adaad4d86ce3acf0d9d0833af717d4029c66140
atharvapathak <atharvapathakb2w@gmail.com>
2025-02-10 13:33:49 +0530

    updates

---
+++ Section 8 - Importing Creating Modules and Packages/main.py
@@ -0,0 +1,12 @@
+# main.py
+
+from mypackage.module1 import add_numbers
+from mypackage.module2 import multiply_numbers
+
+# Using the add_numbers function from module1
+sum_result = add_numbers(5, 7)
+print(f"The sum of 5 and 7 is: {sum_result}")
+
+# Using the multiply_numbers function from module2
+product_result = multiply_numbers(4, 6)
+print(f"The product of 4 and 6 is: {product_result}")
---
+++ Section 8 - Importing Creating Modules and Packages/mypackage/module2.py
@@ -0,0 +1,6 @@
+# module2.py
+
+def multiply_numbers(a, b):
+    return a * b
+
+print(multiply_numbers(5,6))
